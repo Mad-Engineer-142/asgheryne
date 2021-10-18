16fc662e57f3ad5c89e7ac86af5e9b26b48931ce
Yo're-In_David <51287631+Mad-Engineer-142@users.noreply.github.com>
2021-10-18 22:15:25 +0300

    Update appp.py

--- appp.py
+++ appp.py
@@ -19,23 +19,77 @@ def user(user_id):
         print(data)
         return 'aaaaaaaaaa'
     
-@app.route('/api/<uuid>', methods=['GET', 'POST'])
+@app.route('/asd', methods=['POST'])
 def add_message(uuid):
-    if request.method == 'POST':
-        content = request.json
-        print(content)
-        if content['groupId'] == 1:
+    resp = app.response_class(
+                response = json.dumps(   
+        [
+  {
+    "groupId": 1,
+    "raspisanie": [
+      {
+        "dayArray": [
+          {
+            "group": "ХУИКО-021-20",
+            "room": "2601",
+            "subject": "ШИЗИКА",
+            "teacherName": "ВЛАД НА СЕРВЕРЕ КОНЧАЮТСЯ ДЕНЬГИ, Я КОНЕЧНО НЕ НА ЧТО НЕ НАМЕКАЮ НО........",
+            "time": "08.45-10.15",
+            "timeId": 1
+          },
+          {
+            "group": "ХУКО-021-20",
+            "room": "259",
+            "subject": "СОСАТЬ",
+            "teacherName": "Фика Я БРАГО",
+            "time": "14.88-000.00",
+            "timeId": 2
+          },
+          {
+            "group": "ХУКО-021-20",
+            "room": "260",
+            "subject": "ШИЗИКА",
+            "teacherName": "Фика Турицин",
+            "time": "12.40-14.10",
+            "timeId": 3
+          },
+          {
+            "group": "ХУКО-021-20",
+            "room": "777",
+            "subject": "ХУЙ",
+            "teacherName": "ЙЦУЙЦУ",
+            "time": "12.40-14.10",
+            "timeId": 4
+          },
+          {
+            "group": "ХУКО-021-20",
+            "room": "555",
+            "subject": "ШИЗИКА",
+            "teacherName": "И ЭТО НЕ ОБСУЖДАЕТСЯ",
+            "time": "12.40-14.10",
+            "timeId": 5
+          },
+          {
+            "group": "ХУКО-021-20",
+            "room": "1234",
+            "subject": "ШИЗИКА",
+            "teacherName": "АБОБА Я ЕБЛАН СУКА",
+            "time": "14.20-15.50",
+            "timeId": 6
+          }
+        ],
+        "dayId": 1
+      }
+    ]
+  }
+],ensure_ascii=False),
+                mimetype='application/json'
+                )
+        return resp
 
-            aa = jsonify({'groupId': 1,
- 'groupName': 'ЩАКО-01-20(АКС-11)', 
- 'raspisanie': [
-     {'dayArray': [
-         {'group': 'ЩАКО-01-20(АКС-11)', 'room': '123', 'subject': 'физика', 'teacherName': 'ЮРЕЦ КОНЕЧНО', 'time': '8:45-10:30', 'timeId': 1}
-     ], 'dayId': 1}
- ]})
-            return aa
         
-        #return jsonify({"uuid":uuid})
+        
+     
 
 if __name__ == '__main__':
     app.run()
